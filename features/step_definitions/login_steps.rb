b002a6da88b7ca52423cefaf96153c3f5a0f0613
tainguyen14020403 <tainguyen14020403@gmail.com>
2016-04-03 08:34:56 +0000

    cucumber

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -6,13 +6,13 @@ Given (/^Log in$/) do
   password = 'secretpass'
   User.new(:name => name, :email => email, :password => password)
   visit login_path
-  fill_in "user_email", :with => email
-  fill_in "user_password", :with => password
-  click_button "Log in"
+  fill_in "session_email", :with => email
+  fill_in "session_password", :with => password
 end
 When(/^I log in$/) do
+  click_button "Log in"
 end
 
-Then(/^I should see home$/) do
-  pending
+Then(/^I should see homepage$/) do
+  visit root_path
 end
